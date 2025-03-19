03e4ff01cdab0fd75d414fd225020373a2ea2b11
puneet0410 <puneetp2200@gmail.com>
2025-03-19 12:02:50 +0530

    UC_12_RabbitMQ

---
+++ src/main/java/com/AddressBookAppMain/AddressBook/Services/RabbitMQProducer.java
@@ -0,0 +1,19 @@
+package com.AddressBookAppMain.AddressBook.Services;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    private static final String QUEUE_NAME = "addressbook_queue";
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(QUEUE_NAME, message);
+        System.out.println("✅ Sent message: " + message);
+    }
+}
